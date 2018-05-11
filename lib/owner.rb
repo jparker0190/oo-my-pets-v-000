75b6cd15868ee45bd09306afd59931d27fa78980
Joseph Parker <jparker010190@gmail.com>
2018-05-11 03:10:55 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -39,4 +39,16 @@ class Owner
   def walk_dogs
     pets[:dogs].map {|dog| dog.mood = 'happy'}
   end
+  def play_with_cats
+    pets[:cats].map {|cat| cat.mood = 'happy'}
+  end
+  def feed_fish
+    pets[:fishes].map {|fish| fish.mood = 'happy'}
+  end
+  def sell_pets
+    pets.map {|all| all.mood = 'nervous'}
+  end
+  def list_pets
+    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
+  end
 end
