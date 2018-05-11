6bac0e165a67678b62be5fdde82160b91b565e8e
Joseph Parker <jparker010190@gmail.com>
2018-05-11 03:14:06 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -47,9 +47,9 @@ class Owner
   end
   def sell_pets
     pets.each do |type, pets|
-    pets.map {|all| all.mood = "nervous"}
-  end
-  pets.clear
+      pets.map {|pet| pet.mood = "nervous"}
+    end
+    pets.clear
   end
   def list_pets
     "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
