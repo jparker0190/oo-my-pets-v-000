231135355eac8c6e0f7f6fbebe149ee747cade1d
Joseph Parker <jparker010190@gmail.com>
2018-05-11 02:25:35 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,11 +2,8 @@ class Cat
   attr_accessor :cat, :mood
   attr_reader :name
 
-  def initialize(cat, mood = "nervous")
-    @cat = cat
+  def initialize(name, mood = "nervous")
+
     @mood = mood
-  end
-  def initialize(name)
-    @name = name
   end
 end
